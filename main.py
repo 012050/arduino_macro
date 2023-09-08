c3578c2f3e6f631b5db495a05a94149c20fd089a
012050 <beomjunchoe@naver.com>
2023-09-09 00:37:02 +0900

    door, dap, 학생 검색 기능 완성

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import threading
+import tkinter
 from tkinter import simpledialog
 
 import serial
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import tkinter
+from webdriver_manager.chrome import ChromeDriverManager
 
 command = 0
 array = [0, 0, 0, 0]
@@ -91,8 +93,8 @@ def speaker_control():
 def selenium_control():
     global command, array, WindowFullSize
     driver_available = False
-    door = False
-
+    door_login_state = False
+    dap_login_state = False
 
     while True:
         if array == ['7','8','9','#']:
@@ -100,14 +102,21 @@ def selenium_control():
                 driver.close()
             print("selenium control end")
             break
-        elif array == ['7','9','8','#']:
+        elif array == ['7','9','8','#'] or command == "*":
             if driver_available:
+                page_list = driver.window_handles
+                if len(page_list) > 1:
+                    for i in page_list:
+                        driver.switch_to.window(i)
+                        driver.close()
                 driver.close()
+                page_list = []
                 driver_available = False
-            door = False
+            door_login_state = False
+            dap_login_state = False
             command = 0
             array = [0, 0, 0, 0]
-
+            print("all page close")
         if driver_available:
             if WindowFullSize:
                 driver.maximize_window()
@@ -116,55 +125,74 @@ def selenium_control():
         # DOOR
         if command == "1":
             if driver_available == False:
-                driver = webdriver.Chrome()
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
                 driver.set_window_size(984, 945)
                 driver_available = True
-
-            driver.get("https://door.deu.ac.kr/sso/login.aspx")
-            driver.implicitly_wait(60)
-            login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
-            login.send_keys(s_id)
-            pw = driver.find_element(By.CLASS_NAME,"i_text")
-            pw.send_keys(s_pw,Keys.RETURN)
-            driver.implicitly_wait(60)
-            driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
-            driver.implicitly_wait(60)
-            driver.find_element(By.ID, "btn_quick_close").click()
-            driver.implicitly_wait(10)
-            command = 0
-            door = True
-
-        # DAP
-        elif command == "2":
-            if driver_available == False:
-                driver = webdriver.Chrome()
-                driver.set_window_size(984, 945)
-                driver_available = True
-            driver.get("https://dap.deu.ac.kr/sso/login.aspx")
-            driver.implicitly_wait(60)
-            dap_id = driver.find_element(By.XPATH, "/html/body/form/div[3]/div/div[1]/div[2]/input[1]")
-            dap_pw = driver.find_element(By.XPATH, "/html/body/form/div[3]/div/div[1]/div[3]/input")
-            dap_id.send_keys(s_id)
-            dap_pw.send_keys(s_pw, Keys.RETURN)
-            driver.implicitly_wait(60)
-            command = 0
-
-        # SEARCH
-        elif command == "3":
-            if driver_available == False:
-                driver = webdriver.Chrome()
-                driver.set_window_size(984, 945)
-                driver_available = True
-            elif door == False:
+            if door_login_state == False:
                 driver.get("https://door.deu.ac.kr/sso/login.aspx")
                 driver.implicitly_wait(60)
                 login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
                 login.send_keys(s_id)
                 pw = driver.find_element(By.CLASS_NAME,"i_text")
                 pw.send_keys(s_pw,Keys.RETURN)
+                driver.implicitly_wait(60)
+                driver.find_element(By.XPATH,'//*[@id="gnbContent"]/div/div[2]/ol[2]/li[3]/a').click()
+                driver.implicitly_wait(60)
+                driver.find_element(By.ID, "btn_quick_close").click()
+                driver.implicitly_wait(10)
+                door_login_state = True
+            driver.get("http://door.deu.ac.kr/MyPage")
+            command = 0
+
+        # DAP
+        elif command == "2":
+            if driver_available == False:
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+                driver.set_window_size(984, 945)
+                driver_available = True
+            if dap_login_state == False:
+                driver.get("https://dap.deu.ac.kr/sso/login.aspx")
+                driver.implicitly_wait(60)
+                dap_id = driver.find_element(By.XPATH, "/html/body/form/div[3]/div/div[1]/div[2]/input[1]")
+                dap_pw = driver.find_element(By.XPATH, "/html/body/form/div[3]/div/div[1]/div[3]/input")
+                dap_id.send_keys(s_id)
+                dap_pw.send_keys(s_pw, Keys.RETURN)
+                driver.implicitly_wait(60)
+                dap_login_state = True
+            else:
+                driver.get("http://dap.deu.ac.kr/Main.aspx")
+            page_list = driver.window_handles
+            if len(page_list) > 1:
+                for one_page in page_list:
+                    if one_page != page_list[0]:
+                        driver.switch_to.window(one_page)
+                        driver.close()
+                driver.switch_to.window(page_list[0])
+                page_list = []
+            command = 0
+
+        # SEARCH
+        elif command == "3":
+            if driver_available == False:
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+                driver.set_window_size(984, 945)
+                driver_available = True
+            elif door_login_state == False:
+                driver.get("https://door.deu.ac.kr/sso/login.aspx")
+                driver.implicitly_wait(60)
+                login = driver.find_element(By.XPATH,"/html/body/form/div[2]/div[1]/div/table/tbody/tr[1]/td[2]/input")
+                login.send_keys(s_id)
+                pw = driver.find_element(By.CLASS_NAME,"i_text")
+                pw.send_keys(s_pw,Keys.RETURN)
+                door_login_state = True
             driver.get("http://door.deu.ac.kr/Community/MessageSend")
             driver.implicitly_wait(60)
             driver.find_element(By.CSS_SELECTOR, "#popsearch > span > button").click()
+            page_list = driver.window_handles
+            driver.switch_to.window(page_list[0])
+            driver.close()
+            driver.switch_to.window(page_list[1])
+            page_list = []
             command = 0
 
 if __name__ == "__main__":
